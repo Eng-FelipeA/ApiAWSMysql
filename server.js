9912546975de65df589cd2ef00a2ee9ded26e412
Felipe Afonso da Silva Vieira <127235032+Eng-FelipeA@users.noreply.github.com>
2025-07-03 11:00:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ const mysql = require('mysql2/promise');
 const swaggerDocs = require('./swagger');
 const { logInfo, logError } = require('./logger');
 
+const mysql = require('mysql2/promise');
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.DB_PORT || 3306;
+
+const pool = mysql.createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT
+});
+
+
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
